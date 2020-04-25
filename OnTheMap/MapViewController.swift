6442387c8f8adef5777498b177c2a82f727a6ee8
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-25 16:23:18 -0500

    Make MapViewController
    Default pin is animated drop, Udacity blue, and Safari icon
    for RightDetailAccessoryView which then opens the URL in
    a SafariViewController

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Shane Lawson on 4/24/20.
+//  Copyright © 2020 Shane Lawson. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import SafariServices
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+   @IBOutlet weak var mapView: MKMapView!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+      let point = MKPointAnnotation()
+      point.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(exactly: 40.44)!, longitude: CLLocationDegrees(exactly: -99.25)!)
+      point.title = "Some Point"
+      point.subtitle = "http://google.com"
+      mapView.addAnnotation(point)
+      mapView.delegate = self
+   }
+
+   // MARK: - MKMapViewDelegate
+
+   func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+      let reuseId = "pin"
+      if let pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView {
+         pinView.annotation = annotation
+         return Optional(pinView)
+      } else {
+         let initialPinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+         initialPinView.animatesDrop = true
+         initialPinView.pinTintColor = #colorLiteral(red: 0.1648017168, green: 0.7030932307, blue: 0.895126164, alpha: 1)
+         initialPinView.canShowCallout = true
+         let button = UIButton(type: .detailDisclosure)
+         button.setImage(UIImage(systemName: "safari"), for: .normal)
+         initialPinView.rightCalloutAccessoryView = button
+         return Optional(initialPinView)
+      }
+   }
+   
+   func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+      // open url in safari view controller
+      if control == view.rightCalloutAccessoryView {
+         if let toOpen = view.annotation?.subtitle! {
+            let safariVC = SFSafariViewController(url: URL(string: toOpen)!)
+            safariVC.dismissButtonStyle = .close
+            self.present(safariVC, animated: true)
+         }
+      }
+   }
+}
+
