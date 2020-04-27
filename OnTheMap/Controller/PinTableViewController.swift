57dea80c35e040aff188fa488ad1c7a9a4e3f4a8
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-26 23:55:43 -0500

    Open cell subtitle (URL) in safari view controller on tap

--- OnTheMap/Controller/PinTableViewController.swift
+++ OnTheMap/Controller/PinTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import SafariServices
 
 class PinTableViewController: UITableViewController {
 
@@ -16,11 +17,7 @@ class PinTableViewController: UITableViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
 
-      // Uncomment the following line to preserve selection between presentations
-      // self.clearsSelectionOnViewWillAppear = false
-
-      // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-      // self.navigationItem.rightBarButtonItem = self.editButtonItem
+      
    }
 
    // MARK: - UITableViewDataSource
@@ -44,49 +41,13 @@ class PinTableViewController: UITableViewController {
       return cell
    }
 
-   /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-      // Return false if you do not want the specified item to be editable.
-      return true
+   // MARK: - UITableViewDelegate
+   
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      // open url in safari view controller
+      let annotation = annotations[indexPath.row]
+      let safariVC = SFSafariViewController(url: URL(string: annotation.subtitle!)!)
+      safariVC.dismissButtonStyle = .close
+      self.present(safariVC, animated: true)
    }
-   */
-
-   /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-      if editingStyle == .delete {
-         // Delete the row from the data source
-         tableView.deleteRows(at: [indexPath], with: .fade)
-      } else if editingStyle == .insert {
-         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-      }
-   }
-   */
-
-   /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-      
-   }
-   */
-
-   /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-      // Return false if you do not want the item to be re-orderable.
-      return true
-   }
-   */
-
-   /*
-   // MARK: - Navigation
-
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      // Get the new view controller using segue.destination.
-      // Pass the selected object to the new view controller.
-   }
-   */
-
 }
